06455e95318861106c8939550860a846113051f2
JY457 <BarnerZZ@mail.com>
2018-06-01 14:57:04 -0500

    first commit

---
+++ Pyx.py
@@ -0,0 +1,13 @@
+basic_dict = {'key1': 'Val1', 'key2':'Val2'}
+
+nest_dict = {'device1' : {'prompt': 'dev1', 'IP' : '168.0.0.1'} ,
+             'device2' : {'prompt2' : 'dev2', 'IP' : '169.0.0.1'}}
+
+
+basic_list = ['Value1', 'Value2']
+
+nest_list = [['Jar1', 'Jar2'], ['Can1', 'Can2']]
+
+basic_set = {'dog', 'cat'}          # No repeats, non-indexed
+
+basic_touple = ('val1', 'val2')     # Cannot be modified after creation
